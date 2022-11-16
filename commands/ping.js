760c623fef8514cfb68a524b1f6d499f4b2b2023
TC6IDM <TCAND6@GMAIL.COM>
2022-11-16 02:50:30 -0500

    Update ping.js
    adds the latency (ms) to the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		return interaction.reply(`Pong!`);
+		await interaction.reply(`Pong!`);
+		interaction.editReply({ content: `Pong! \`${Date.now() - interaction.createdTimestamp}ms\`` });
 	},
 };
